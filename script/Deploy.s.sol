e1c71e6d25f837ec9c3ade2000fabc24b992f6d6
Kaspar Kallas <kasparkallas@outlook.com>
2023-12-14 11:47:55 +0000

    fix deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity =0.8.15;
 
-import { Foo } from "../src/Foo.sol";
+import { IUniswapSwapRouter, UniswapLiquidityMover } from "../src/LiquidityMover.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcast returns (Foo foo) {
-        foo = new Foo();
+    function run() public broadcast returns (UniswapLiquidityMover liquidityMover) {
+        liquidityMover = new UniswapLiquidityMover(IUniswapSwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564));
     }
 }
