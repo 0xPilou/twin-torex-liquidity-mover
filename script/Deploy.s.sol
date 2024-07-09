bac651f82ce4d7ccee3e4f932df67a45d48abdd7
Kaspar Kallas <git@kasparkallas.com>
2024-07-09 17:08:48 +0300

    refactor LM to support both withProfit and withoutProfit

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,16 +4,16 @@ pragma solidity =0.8.15;
 import { ISETH } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/tokens/ISETH.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import { IUniswapSwapRouter } from "../src/LiquidityMover.sol";
+import { IUniswapSwapRouter } from "../src/ILiquidityMover.sol";
 
-import { NonprofitUniswapLiquidityMover as UniswapLiquidityMover } from "../src/NonprofitUniswapLiquidityMover.sol";
+import { SwapRouter02LiquidityMover } from "../src/SwapRouter02LiquidityMover.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcast returns (UniswapLiquidityMover liquidityMover) {
-        liquidityMover = new UniswapLiquidityMover(
+    function run() public broadcast returns (SwapRouter02LiquidityMover liquidityMover) {
+        liquidityMover = new SwapRouter02LiquidityMover(
             // IUniswapSwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564),
             // ISETH(0x96B82B65ACF7072eFEb00502F45757F254c2a0D4)
             IUniswapSwapRouter(0x2626664c2603336E57B271c5C0b26F421741e481),
--- test/2024-02-28-optimism.t.sol
+++ test/2024-02-28-optimism.t.sol
@@ -9,14 +9,14 @@ import { StdCheats } from "forge-std/StdCheats.sol";
 import { ISETH } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/tokens/ISETH.sol";
 import { IWETH9 } from "@uniswap/v3-periphery/contracts/interfaces/external/IWETH9.sol";
 
-import { ILiquidityMover, IUniswapSwapRouter, Torex, Config } from "../src/LiquidityMover.sol";
+import { ILiquidityMover, IUniswapSwapRouter, Torex, TorexConfig } from "../src/ILiquidityMover.sol";
 
-import { NonprofitUniswapLiquidityMover as UniswapLiquidityMover } from "../src/NonprofitUniswapLiquidityMover.sol";
+import { SwapRouter02LiquidityMover } from "../src/SwapRouter02LiquidityMover.sol";
 
 import { ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperToken.sol";
 
 contract LiquidityMoverTests is PRBTest {
-    UniswapLiquidityMover internal sut;
+    SwapRouter02LiquidityMover internal sut;
 
     IERC20 internal constant OPx = IERC20(0x1828Bff08BD244F7990edDCd9B19cc654b33cDB4);
     IERC20 internal constant OP = IERC20(0x4200000000000000000000000000000000000042);
@@ -32,7 +32,7 @@ contract LiquidityMoverTests is PRBTest {
             blockNumber: 116_756_644
         });
 
-        sut = new UniswapLiquidityMover(
+        sut = new SwapRouter02LiquidityMover(
             IUniswapSwapRouter(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), // "SwapRouter02"! (not just "SwapRouter")
             ISETH(0x4ac8bD1bDaE47beeF2D1c6Aa62229509b962Aa0d), // The Super Token on for Native Asset on Optimism
             IERC20(address(0))
@@ -57,7 +57,7 @@ contract LiquidityMoverTests is PRBTest {
     }
 
     function _testTorex(Torex torex, address rewardToken) internal {
-        assertTrue(sut.moveLiquidity(torex));
+        sut.moveLiquidity(torex);
 
         ISuperToken outToken = torex.getConfig().outToken;
         assertEq(outToken.balanceOf(address(sut)), 0);
